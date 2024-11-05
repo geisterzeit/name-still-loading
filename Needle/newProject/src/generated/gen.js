22b7a28144033ec18c5a36f34f77e919c432156c
Julian Meser <meser@posteo.de>
2024-11-05 16:41:29 +0100

    Initial commit

---
+++ Needle/newProject/src/generated/gen.js
@@ -0,0 +1,16 @@
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+needle_exported_files.push("assets/scene.glb?v=1730819690130");
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1730819690130");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.48\.3");
