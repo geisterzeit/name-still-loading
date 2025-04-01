aab2a5a092fb19f5fe64ca6d7ce5f8a150cd56b7
Julian Meser <meser@posteo.de>
2025-04-01 13:19:09 +0200

    Fixed tower rotation and fire tower draggable bug

--- Needle/newProject/src/scripts/Targeting.ts
+++ Needle/newProject/src/scripts/Targeting.ts
@@ -1,9 +1,12 @@
 import { Behaviour, GameObject } from "@needle-tools/engine";
+import { Vector3 } from "three";
 
 export class Targeting extends Behaviour {
     target(firstEnemy: GameObject) {
-        let target = firstEnemy.worldPosition
-        target.x = 0
+        let target = new Vector3()
+            .subVectors(firstEnemy.position, this.gameObject.position)
+
+        target.y = 0.3
         this.gameObject.lookAt(target)
     }
 }
